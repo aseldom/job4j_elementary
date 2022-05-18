27c4a84f35ed35376e923e93397b51673dff5879
Alexander <aseldom@yandex.ru>
2022-05-18 16:35:47 +0700

    Что такое import [#504773 #301994]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -9,7 +9,7 @@ public class MathFunction {
     public static double multiply(double first, double second) {
         return first * second;
     }
-    
+
     public static double divide(double first, double second) {
         return first / second;
     }

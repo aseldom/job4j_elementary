2d5fac9bd3b45d152678b5d69555a2e8726acd31
Alexander <aseldom@yandex.ru>
2022-05-26 09:52:20 +0700

    Жадный алгоритм. Сдача в кофе машине [#34741 #304918]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -5,22 +5,14 @@ import java.util.Arrays;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
         int change = money - price;
-        int coins10 = change / coins[0];
-        int coins5 = (change % coins[0]) / coins[1];
-        int coins2 = (change % coins[0] % coins[1]) / coins[2];
-        int coins1 = change % coins[0] % coins[1] % coins[2];
-        int size = coins10 + coins5 + coins2 + coins1;
-        int[] rsl = new int[size];
-        for (int i = 0; i < size; i++) {
-            if (i < coins10) {
-                rsl[i] = coins[0];
-            } else if (i < coins10 + coins5) {
-                rsl[i] = coins[1];
-            } else if (i < coins10 + coins5 + coins2) {
-                rsl[i] = coins[2];
-            } else {
-                rsl[i] = coins[3];
+        for (int coin : coins) {
+            while (coin <= change) {
+                rsl[size] = coin;
+                change -= coin;
+                size++;
             }
         }
         return Arrays.copyOf(rsl, size);
